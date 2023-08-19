80b1d79d33b2a783f27d6f0a0f84f9c9d5ef5d48
Marcelo Saavedra <138951106+hchelo@users.noreply.github.com>
2023-08-19 18:14:49 -0400

    Update C1_Iris_Bayes.py

--- classification/C1_Iris_Bayes.py
+++ classification/C1_Iris_Bayes.py
@@ -1,3 +1,7 @@
+# Marcelo Saavedra Alcoba
+# Curso Ciencia de Datos Unifranz
+# Clasificador Naive Bayes 
+
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
